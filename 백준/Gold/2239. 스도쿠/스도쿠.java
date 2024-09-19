9ca55600c4d50b4724809da54deed5b1df87255f
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-09-19 15:34:57 +0900

    [Gold IV] Title: 스도쿠, Time: 652 ms, Memory: 21684 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	static private class Point {
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static final int N = 9;
+	static int[][] map;
+	static ArrayList<Point> zero;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		map = new int[N][N];
+		zero = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			String S = br.readLine();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = S.charAt(j) - '0'; // 수정된 부분: 문자를 숫자로 변환
+				if (map[i][j] == 0)
+					zero.add(new Point(i, j));
+			}
+		}
+		recur(0);
+	}
+
+	private static void recur(int cnt) {
+		if (cnt == zero.size()) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(map[i][j]); // 출력 시 공백 추가
+				}
+				System.out.println();
+			}
+			System.exit(0);
+
+		}
+		for (int i = 1; i <= 9; i++) {
+			if (check(zero.get(cnt).x, zero.get(cnt).y, i)) {
+				map[zero.get(cnt).x][zero.get(cnt).y] = i;
+				recur(cnt + 1);
+				map[zero.get(cnt).x][zero.get(cnt).y] = 0;
+			}
+		}
+	}
+
+	private static boolean check(int x, int y, int num) {
+		// 가로 검사
+		for (int i = 0; i < N; i++) {
+			if (map[x][i] == num)
+				return false;
+		}
+		// 세로 검사
+		for (int i = 0; i < N; i++) {
+			if (map[i][y] == num)
+				return false;
+		}
+		// 3x3 박스 검사
+		int startX = (x / 3) * 3;
+		int startY = (y / 3) * 3;
+		for (int i = startX; i < startX + 3; i++) {
+			for (int j = startY; j < startY + 3; j++) {
+				if (map[i][j] == num)
+					return false;
+			}
+		}
+
+		return true;
+	}
+}
