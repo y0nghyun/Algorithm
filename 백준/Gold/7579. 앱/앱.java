71474410375ffcddb55cb9cdc893e37cceabb834
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-09-30 14:52:28 +0900

    [Gold III] Title: 앱, Time: 120 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -23,26 +22,28 @@ public class Main {
 		for (int i = 1; i <= N; i++) {
 			memory[i][1] = Integer.parseInt(st.nextToken());
 		}
-		int sum = 0;
 		int csum = 0;
 		for (int i = 1; i <= N; i++) {
-			sum += memory[i][0];
 			csum += memory[i][1];
 		}
-		int[] dp = new int[sum + 1];
 
-		Arrays.fill(dp, csum);
+		int[] dp = new int[csum + 1];
+
 		for (int i = 1; i <= N; i++) {
-			for (int j = sum; j >= memory[i][0]; j--) {
-				dp[j] = Math.min(dp[j - memory[i][0]] + memory[i][1], dp[j]);
+			for (int j = csum; j >= memory[i][1]; j--) {
+				dp[j] = Math.max(dp[j - memory[i][1]] + memory[i][0], dp[j]);
 			}
-			dp[memory[i][0]] = Math.min(dp[memory[i][0]], memory[i][1]);
 		}
-		int min = csum;
-		for (int i = M; i <= sum; i++) {
-			csum = Math.min(csum, dp[i]);
+
+		int ans = csum;
+		for (int i = 0; i <= csum; i++) {
+			if (dp[i] >= M) {
+				ans = i;
+				break;
+			}
 		}
-		System.out.println(csum);
+
+		System.out.println(ans);
 	}
 
 }
