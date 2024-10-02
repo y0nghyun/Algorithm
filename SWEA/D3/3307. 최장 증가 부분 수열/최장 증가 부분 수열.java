e6fee117ed8d5eb5bef6e3386121a92500bc9988
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-10-02 15:53:43 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 162 ms, Memory: 27,556 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -14,24 +14,23 @@ public class Solution {
 			int n = Integer.parseInt(br.readLine());
 			int[] num = new int[n];
 			int[] dp = new int[n];
-
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < n; i++)
 				num[i] = Integer.parseInt(st.nextToken());
 
 			dp[0] = 1;
+			int max = 1;
 			for (int i = 1; i < num.length; i++) {
 				for (int j = i - 1; j >= 0; j--) {
-					if (num[i] > num[j] && dp[j] + 1 > dp[i])
+					if (num[i] > num[j] && dp[j] + 1 > dp[i]) {
 						dp[i] = dp[j] + 1;
+						if (max < dp[i])
+							max = dp[i];
+					}
 				}
 				if (dp[i] == 0)
 					dp[i] = 1;
 			}
-			int max = 0;
-			for (int i = 0; i < dp.length; i++)
-				if (max < dp[i])
-					max = dp[i];
 			System.out.println("#" + TC + " " + max);
 		}
 	}
