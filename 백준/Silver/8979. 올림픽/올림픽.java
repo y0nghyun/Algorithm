512f94e11d24567e3320a4bccc216756cb9634c1
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-10-17 21:50:05 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14676 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,44 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int K;
+	static class Country implements Comparable<Country> {
+		int num, gold, silver, bronze;
 
-	static int[] countrys;
+		public Country(int num, int gold, int silver, int bronze) {
+			this.num = num;
+			this.gold = gold;
+			this.silver = silver;
+			this.bronze = bronze;
+		}
+
+		@Override
+		public int compareTo(Country o) {
+			if (this.gold != o.gold) {
+				return o.gold - this.gold;
+			}
+			if (this.silver != o.silver) {
+				return o.silver - this.silver;
+			}
+			if (this.bronze != o.bronze) {
+				return o.bronze - this.bronze;
+			}
+			return 0;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		List<Country> countries = new ArrayList<>();
+		Country target = null;
 
-		int targetScore = 0;
-		countrys = new int[N + 1];
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int num = Integer.parseInt(st.nextToken());
 			int gold = Integer.parseInt(st.nextToken());
 			int silver = Integer.parseInt(st.nextToken());
 			int bronze = Integer.parseInt(st.nextToken());
-
-			int score = gold * 100 + silver * 10 + bronze;
-			if (num == K)
-				targetScore = score;
-			countrys[num] = score;
+			Country country = new Country(num, gold, silver, bronze);
+			countries.add(country);
+			if (num == k) {
+				target = country;
+			}
 		}
 
-		Arrays.sort(countrys);
+		Collections.sort(countries);
 
-		for (int i = N; i > 0; --i) {
-			if (countrys[i] == targetScore) {
-				System.out.println(N - i + 1);
-				return;
+		int rank = 1;
+		int sameRankCount = 1;
+
+		for (int i = 0; i < countries.size(); i++) {
+			if (i > 0 && countries.get(i).compareTo(countries.get(i - 1)) != 0) {
+				rank += sameRankCount;
+				sameRankCount = 1;
+			} else if (i > 0) {
+				sameRankCount++;
+			}
+
+			if (countries.get(i).num == target.num) {
+				System.out.println(rank);
+				break;
 			}
 		}
 	}
