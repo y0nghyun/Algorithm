bb10ec3c0c779ba5acd1cdccea3e8f2281602721
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-12-09 16:08:22 +0900

    [Silver I] Title: 회전 초밥, Time: 536 ms, Memory: 18788 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -11,8 +11,10 @@ public class Main {
 	static int k;
 	static int c;
 
+	// 전체 초밥 리스트
 	static int[] belt;
-	static boolean[] check;
+	// 이미 먹은 초밥인지 여부
+	static boolean[] isEaten;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,27 +26,33 @@ public class Main {
 		c = Integer.parseInt(st.nextToken());
 
 		belt = new int[n];
-		check = new boolean[d + 1];
+		isEaten = new boolean[d + 1];
 		for (int i = 0; i < n; i++) {
 			belt[i] = Integer.parseInt(br.readLine());
 		}
 
 		int max = 0;
+		// 접시 개수만큼
 		for (int i = 0; i < n; i++) {
 			int cnt = 0;
+			// 연속해서 먹는 접시의 수만큼
 			for (int j = 0; j < k; j++) {
 				int temp = i + j;
+				// 만약 배열을 벗어나면 다시 앞으로
 				if (temp >= n)
 					temp -= n;
-				if (!check[belt[temp]]) {
+				// 아직 먹지 않은 초밥이라면 개수 ++ , 먹었다고 체크
+				if (!isEaten[belt[temp]]) {
 					cnt++;
-					check[belt[temp]] = true;
+					isEaten[belt[temp]] = true;
 				}
 			}
-			if (!check[c])
+			// 쿠폰 사용
+			if (!isEaten[c])
 				cnt++;
 			max = Math.max(cnt, max);
-			Arrays.fill(check, false);
+			// 먹었는지 여부 초기화
+			Arrays.fill(isEaten, false);
 		}
 		System.out.println(max);
 	}
