6ccf1cb3c0badf36ba227b65265198be43660bb9
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2024-12-20 17:02:43 +0900

    [Gold II] Title: 동전 분배, Time: 2628 ms, Memory: 18384 KB -BaekjoonHub

---
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int sum;
+	static int[] coins;
+	static boolean[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int T = 0; T < 3; T++) {
+			N = Integer.parseInt(br.readLine());
+
+			sum = 0;
+			coins = new int[100001];
+
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int coin = Integer.parseInt(st.nextToken());
+				int cnt = Integer.parseInt(st.nextToken());
+				coins[coin] = cnt;
+				sum += coin * cnt;
+			}
+
+			if (sum % 2 != 0) {
+				System.out.println(0);
+				continue;
+			}
+
+			sum /= 2;
+			dp = new boolean[sum + 1];
+			dp[0] = true;
+
+			for (int i = 1; i <= sum; i++) {
+				if (coins[i] > 0) {
+					int coin = i;
+					int cnt = coins[i];
+					for (int j = sum; j >= coin; j--) {
+						for (int k = 1; k <= cnt && j >= k * coin; k++) {
+							if (dp[j - k * coin]) {
+								dp[j] = true;
+							}
+						}
+					}
+				}
+			}
+
+			if (dp[sum]) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+		}
+	}
+}
