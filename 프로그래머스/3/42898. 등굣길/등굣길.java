40d75b6803a5fe72dcc2c8e6eaf1733bd4f227b5
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2025-08-26 15:55:20 +0900

    [level 3] Title: 등굣길, Time: 0.74 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,33 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+        boolean[][] blocked = new boolean[n][m];
+
+        for (int[] p : puddles) {
+            int y = p[1] - 1;
+            int x = p[0] - 1;
+            blocked[y][x] = true;
+        }
+
+        if (blocked[0][0]) return 0;
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) continue;
+                if (blocked[i][j]) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                long up = (i > 0) ? dp[i - 1][j] : 0;
+                long left = (j > 0) ? dp[i][j - 1] : 0;
+                dp[i][j] = (int) ((up + left) % MOD);
+            }
+        }
+
+        return dp[n - 1][m - 1];
+    }
+}
