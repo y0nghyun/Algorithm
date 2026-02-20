b5af8ce8a33ddceebbbf971abf69107e97cb5c61
y0nghyun <86294007+y0nghyun@users.noreply.github.com>
2026-02-20 21:24:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        while(true){
+            if(a%2 == 1 && a+1 == b)
+                break;
+            if(b%2 == 1 && b+1 == a)
+                break;
+            a = a%2==0?a/2:a/2+1;
+            b = b%2==0?b/2:b/2+1;
+            
+            answer++;
+        }
+        return answer;
+    }
+}
